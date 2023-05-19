e192d15a608b4a0d07199f23744e7ebaeb8dc8b1
shojol021 <shojol021@gmail.com>
2023-05-19 14:23:00 +0600

    all toys and single toy data get done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const app = express()
 const port = process.env.port || 3000
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -24,6 +24,20 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const toysCollection = client.db('ToyDB').collection('toys')
+
+        app.get('/toys', async(req, res) => {
+            const result = await toysCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.get('/toys/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await toysCollection.findOne(query)
+            res.send(result)
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
