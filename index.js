e22d4c164b7a3c26fa95f5e66e6d820a3078f0f3
shojol021 <shojol021@gmail.com>
2023-05-20 16:47:40 +0600

    query with email data for my toys

--- index.js
+++ index.js
@@ -23,12 +23,17 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+         await client.connect();
 
         const toysCollection = client.db('ToyDB').collection('toys')
 
         app.get('/toys', async(req, res) => {
-            const result = await toysCollection.find().toArray()
+            console.log(req.query.sellerEmail)
+            let query = {}
+            if(req.query.sellerEmail){
+                query = {sellerEmail: req.query.sellerEmail}
+            }
+            const result = await toysCollection.find(query).toArray()
             res.send(result)
         })
 
