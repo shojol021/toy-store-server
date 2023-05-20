2f9e6c9ce4b20fa5ab3698e8b398031c0f9a7f97
shojol021 <shojol021@gmail.com>
2023-05-20 18:05:38 +0600

    delete my toys api done

--- index.js
+++ index.js
@@ -50,6 +50,14 @@ async function run() {
             const result = await toysCollection.insertOne(toyDetails)
             res.send(result)
         })
+
+        app.delete('/delete/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await toysCollection.deleteOne(query)
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
