7cf37517fed019463e3d31850263fc15d22dc689
shojol021 <shojol021@gmail.com>
2023-05-21 13:48:28 +0600

    vercel put method updated

--- index.js
+++ index.js
@@ -5,13 +5,7 @@ const port = process.env.port || 3000
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-const corsOptions ={
-    origin:'*',
-    credentials:true,
-    optionSuccessStatus:200,
-    }
-    app.use(cors(corsOptions))
-
+app.use(cors())
 app.use(express.json())
 
 
@@ -29,56 +23,64 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-         await client.connect();
+        //  await client.connect();
 
         const toysCollection = client.db('ToyDB').collection('toys')
 
-        app.get('/toys', async(req, res) => {
+        app.get('/toys', async (req, res) => {
             console.log(req.query.sellerEmail)
+            console.log(req.body)
             let query = {}
-            if(req.query.sellerEmail){
-                query = {sellerEmail: req.query.sellerEmail}
+            if (req.query.sellerEmail) {
+                query = { sellerEmail: req.query.sellerEmail }
+                const result = await toysCollection.find(query).toArray()
+                res.send(result)
             }
-            const result = await toysCollection.find(query).toArray()
-            res.send(result)
+            else{
+                query = { search: req.query.search}
+            }
+
         })
 
-        app.get('/toys/:id', async(req, res) => {
+        app.get('/toys/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await toysCollection.findOne(query)
             res.send(result)
         })
 
-        app.post('/toys', async(req, res) => {
+        app.post('/toys', async (req, res) => {
             const toyDetails = req.body;
             console.log(toyDetails)
             const result = await toysCollection.insertOne(toyDetails)
             res.send(result)
         })
 
-        app.put('/toys/:id', async(req, res) => {
+        app.put('/toys/:id', async (req, res) => {
             const id = req.params.id;
             const updatedDetails = req.body;
             console.log(updatedDetails)
-            // const filter = {_id: id}
-            // const updatedDoc = {
-            //     Set$: {
-            //         price: updatedDetails.price
-            //     }
-            // }
-            // const result = await toysCollection.updateOne(filter)
+            const filter = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    price: updatedDetails.price,
+                    availableQuantity: updatedDetails.availableQuantity,
+                    shortDescription: updatedDetails.shortDescription
+                }
+            }
+            const result = await toysCollection.updateOne(filter, updatedDoc)
+            res.send(result)
         })
 
-        app.delete('/delete/:id', async(req, res) => {
+        app.delete('/delete/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await toysCollection.deleteOne(query)
             res.send(result)
         })
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     }
     finally {
